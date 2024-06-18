950aadab176f2b27eb8a2536ef338493df72f49f
Francisco Mendes <fmendes@mosano.eu>
2024-06-18 11:50:01 +0100

    metro.config.js fixed import statement

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
-import { getDefaultConfig, mergeConfig } from '@react-native/metro-config';
+// eslint-disable-next-line
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
 const { assetExts, sourceExts } = defaultConfig.resolver;
@@ -19,4 +20,4 @@ const config = {
   },
 };
 
-export default mergeConfig(defaultConfig, config);
+module.exports = mergeConfig(defaultConfig, config);
