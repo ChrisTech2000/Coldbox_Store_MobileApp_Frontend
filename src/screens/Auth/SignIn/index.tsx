dcff9e2ce7ca9ee86a57c4fca9031128f246f736
sverissimo <sverissimo@telemedicineclic.com>
2024-06-20 18:34:04 +0000

    feat: added form validation to sign in screen

--- src/screens/Auth/SignIn/index.tsx
+++ src/screens/Auth/SignIn/index.tsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React, { useCallback } from 'react';
 import { Dimensions, Text, View } from 'react-native';
 import { Divider, TextInput } from 'react-native-paper';
+import { Controller, useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 import { Button } from '#ui/components/Button';
 import type { AuthRouteProps } from '#navigation/Auth';
@@ -30,9 +33,57 @@ const ACCOUNT_DESCRIPTIONS = {
     'The cold room user. Farmers, traders, retailers who have access to a smartphone can log in here. Cold room users without a smartphone can access the information of the app by visiting a cold room and interacting with the operator.',
 };
 
+const schema = z
+  .object({
+    activeProfile: z.enum([
+      EAccountProfile.EMPLOYEE,
+      EAccountProfile.FARMER,
+      EAccountProfile.OPERATOR,
+    ]),
+    user: z.string(),
+    password: z.string().min(1, {
+      message: 'Password is required.',
+    }),
+  })
+  .superRefine((data, ctx) => {
+    if (data.user.length < 1) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.too_small,
+        path: ['user'],
+        minimum: 1,
+        type: 'string',
+        inclusive: true,
+        message:
+          data.activeProfile === EAccountProfile.EMPLOYEE
+            ? 'An email or phone number is required.'
+            : 'Phone number is required',
+      });
+    }
+  });
+
 function SignIn(props: AuthRouteProps<'SignIn'>) {
   const { navigation } = props;
-  const [activeProfile, setActiveProfile] = useState<EAccountProfile>(EAccountProfile.EMPLOYEE);
+
+  const {
+    control,
+    handleSubmit,
+    watch,
+    clearErrors,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      activeProfile: EAccountProfile.EMPLOYEE,
+      user: '',
+      password: '',
+    },
+  });
+
+  const activeProfile = watch('activeProfile');
+
+  const onSubmit = useCallback(() => {
+    // TODO: Implement API call here
+  }, []);
 
   return (
     <View tw="flex-1 items-center justify-center">
@@ -40,31 +91,68 @@ function SignIn(props: AuthRouteProps<'SignIn'>) {
       <Text tw="mb-2 text-xl font-bold">Sign In</Text>
       <View tw="w-full flex flex-row justify-between mb-2 px-4">
         <View tw="items-center">
-          <AccountCard
-            isActive={activeProfile === EAccountProfile.EMPLOYEE}
-            onPress={() => setActiveProfile(EAccountProfile.EMPLOYEE)}
-          >
-            <Employee width={ACCOUNT_TYPE_SIZE} height={ACCOUNT_TYPE_SIZE} />
-          </AccountCard>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange } }) => (
+              <AccountCard
+                isActive={activeProfile === EAccountProfile.EMPLOYEE}
+                onPress={() => {
+                  clearErrors();
+                  onChange(EAccountProfile.EMPLOYEE);
+                }}
+              >
+                <Employee width={ACCOUNT_TYPE_SIZE} height={ACCOUNT_TYPE_SIZE} />
+              </AccountCard>
+            )}
+            name="activeProfile"
+          />
           <Text tw="mt-4 text-xs">Registered</Text>
           <Text tw="text-xs">Employee</Text>
         </View>
         <View tw="items-center">
-          <AccountCard
-            isActive={activeProfile === EAccountProfile.OPERATOR}
-            onPress={() => setActiveProfile(EAccountProfile.OPERATOR)}
-          >
-            <Operator width={ACCOUNT_TYPE_SIZE} height={ACCOUNT_TYPE_SIZE} />
-          </AccountCard>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange } }) => (
+              <AccountCard
+                isActive={activeProfile === EAccountProfile.OPERATOR}
+                onPress={() => {
+                  clearErrors();
+                  onChange(EAccountProfile.OPERATOR);
+                }}
+              >
+                <Operator width={ACCOUNT_TYPE_SIZE} height={ACCOUNT_TYPE_SIZE} />
+              </AccountCard>
+            )}
+            name="activeProfile"
+          />
           <Text tw="mt-4 text-xs">Operator</Text>
         </View>
         <View tw="items-center">
-          <AccountCard
-            isActive={activeProfile === EAccountProfile.FARMER}
-            onPress={() => setActiveProfile(EAccountProfile.FARMER)}
-          >
-            <Farmer width={ACCOUNT_TYPE_SIZE} height={ACCOUNT_TYPE_SIZE} />
-          </AccountCard>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange } }) => (
+              <AccountCard
+                isActive={activeProfile === EAccountProfile.FARMER}
+                onPress={() => {
+                  clearErrors();
+                  onChange(EAccountProfile.FARMER);
+                }}
+              >
+                <Farmer width={ACCOUNT_TYPE_SIZE} height={ACCOUNT_TYPE_SIZE} />
+              </AccountCard>
+            )}
+            name="activeProfile"
+          />
+
           <Text tw="mt-4 text-xs">Cooling</Text>
           <Text tw="text-xs">User</Text>
         </View>
@@ -74,32 +162,61 @@ function SignIn(props: AuthRouteProps<'SignIn'>) {
       <Text tw="text-xs max-w-[95%]">{ACCOUNT_DESCRIPTIONS[activeProfile]}</Text>
       <Divider tw="w-full my-2" />
 
-      {/** @NOTE that currently this component is responsible for an error being thrown. There is already a PR that should be included in the next version */}
-      <TextInput
-        tw="w-[95%] px-4 bg-white border rounded-sm mb-2 h-12"
-        label={`${activeProfile === EAccountProfile.EMPLOYEE && 'Email/'}Phone Number`}
-        left={<TextInput.Icon icon="phone" />}
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, value } }) => (
+          <TextInput
+            tw="w-[95%] px-4 bg-white border rounded-sm mb-2 h-12"
+            label={`${activeProfile === EAccountProfile.EMPLOYEE ? 'Email/' : ''}Phone Number`}
+            left={<TextInput.Icon icon="phone" />}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="user"
       />
-      <Text tw="text-xs w-[95%] mb-2 px-3">
+      {errors.user && (
+        <Text tw="text-xs text-red-600 mt-[-2] pl-3 w-[95%]">
+          {errors.user.message?.toString()}
+        </Text>
+      )}
+      <Text tw="text-xs w-[95%] my-2 px-3">
         Please provide valid {activeProfile === EAccountProfile.EMPLOYEE ? 'email/' : ''}phone
         number (with country code).{' '}
       </Text>
-      <TextInput
-        tw="w-[95%] px-4 bg-white border rounded-sm mb-2 h-12"
-        label="Password"
-        secureTextEntry
-        right={<TextInput.Icon icon="eye" />}
-        left={<TextInput.Icon icon="lock" />}
+
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, value } }) => (
+          <TextInput
+            tw="w-[95%] px-4 bg-white border rounded-sm mb-2 h-12"
+            label="Password"
+            secureTextEntry
+            right={<TextInput.Icon icon="eye" />}
+            left={<TextInput.Icon icon="lock" />}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="password"
       />
+      {errors.password && (
+        <Text tw="text-xs text-red-600 mt-[-2] mb-2 pl-3 w-[95%]">
+          {errors.password.message?.toString()}
+        </Text>
+      )}
 
       <Button
         tw="w-[95%] border-2 rounded-sm mb-2"
         mode="contained"
         uppercase
-        onPress={(evt) => {
-          evt.stopPropagation();
-          // TODO: implement API call
-        }}
+        onPress={handleSubmit(onSubmit)}
       >
         Log In
       </Button>
