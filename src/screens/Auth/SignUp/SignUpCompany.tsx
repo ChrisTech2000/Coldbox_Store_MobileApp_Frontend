98c50ddbe463aecab7fcdb2def419ede1ea55911
sverissimo <sverissimo@telemedicineclic.com>
2024-06-25 15:49:09 +0000

    feat: created dedicated textInput component

--- src/screens/Auth/SignUp/SignUpCompany.tsx
+++ src/screens/Auth/SignUp/SignUpCompany.tsx
@@ -8,13 +8,14 @@ import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view
 import { Checkbox, Text, TextInput } from 'react-native-paper';
 import { z } from 'zod';
 
+import { EGender } from '#types/auth';
 import { Button } from '#ui/components/Button';
+import { Input } from '#ui/components/Input';
 import { withSafeArea } from '#ui/primitives/withSafeArea';
 import { SignUpFormSelectLg } from './components/SignUpFormSelectLg';
 import { SignUpFormSelectMd } from './components/SignUpFormSelectMd';
-import { customCountrySort } from './utils';
-import { EGender } from '#types/auth';
 import { SignUpAsCompanySchema } from './schemas';
+import { customCountrySort } from './utils';
 
 const allCountries = getAllISOCodes();
 const allCountryNames = allCountries.map((code) => code.countryName);
@@ -116,21 +117,16 @@ function SignUpCompany() {
           required: true,
         }}
         render={({ field: { onChange, value } }) => (
-          <TextInput
+          <Input
             tw="w-full text-base bg-white rounded-sm mb-3 h-12"
             label={'Company Name*'}
             onChangeText={onChange}
             value={value}
-            error={!!errors.companyName}
+            error={errors.companyName}
           />
         )}
         name="companyName"
       />
-      {errors.companyName && (
-        <Text tw="text-xs text-red-600 mt-[-2] mb-2 pl-3 w-[95%]">
-          {errors.companyName.message?.toString()}
-        </Text>
-      )}
 
       {/** COUNTRY */}
       <SignUpFormSelectLg<SignUpSchemaType>
@@ -179,21 +175,16 @@ function SignUpCompany() {
           required: true,
         }}
         render={({ field: { onChange, value } }) => (
-          <TextInput
+          <Input
             tw="w-full text-base bg-white rounded-sm mb-2 h-12"
             label={'Email*'}
             onChangeText={onChange}
             value={value}
-            error={!!errors.email}
+            error={errors.email}
           />
         )}
         name="email"
       />
-      {errors.email && (
-        <Text tw="text-xs text-red-600 mt-[-2] mb-2 pl-3 w-[95%]">
-          {errors.email.message?.toString()}
-        </Text>
-      )}
 
       {/** FIRST NAME */}
       <Controller
@@ -202,21 +193,16 @@ function SignUpCompany() {
           required: true,
         }}
         render={({ field: { onChange, value } }) => (
-          <TextInput
+          <Input
             tw="w-full text-base bg-white rounded-sm mb-2 h-12"
             label={'First Name*'}
             onChangeText={onChange}
             value={value}
-            error={!!errors.firstName}
+            error={errors.firstName}
           />
         )}
         name="firstName"
       />
-      {errors.firstName && (
-        <Text tw="text-xs text-red-600 mt-[-2] mb-2 pl-3 w-[95%]">
-          {errors.firstName.message?.toString()}
-        </Text>
-      )}
 
       {/** LAST NAME */}
       <Controller
@@ -225,21 +211,16 @@ function SignUpCompany() {
           required: true,
         }}
         render={({ field: { onChange, value } }) => (
-          <TextInput
+          <Input
             tw="w-full text-base bg-white rounded-sm mb-2 h-12"
             label={'Last Name*'}
             onChangeText={onChange}
             value={value}
-            error={!!errors.lastName}
+            error={errors.lastName}
           />
         )}
         name="lastName"
       />
-      {errors.lastName && (
-        <Text tw="text-xs text-red-600 mt-[-2] mb-2 pl-3 w-[95%]">
-          {errors.lastName.message?.toString()}
-        </Text>
-      )}
 
       {/** GENDER */}
       <SignUpFormSelectMd
@@ -264,12 +245,12 @@ function SignUpCompany() {
       <Controller
         control={control}
         render={({ field: { onChange, value } }) => (
-          <TextInput
+          <Input
             tw="w-full text-base bg-white rounded-sm mb-2 h-12"
             label={'Phone Number (with country code)'}
             onChangeText={onChange}
             value={value}
-            error={!!errors.phone}
+            error={errors.phone}
           />
         )}
         name="phone"
@@ -282,13 +263,13 @@ function SignUpCompany() {
           required: true,
         }}
         render={({ field: { onChange, value } }) => (
-          <TextInput
+          <Input
             tw="w-full text-base bg-white rounded-sm mb-2 h-12"
             label={'Password*'}
             onChangeText={onChange}
             value={value}
             secureTextEntry={hidePass}
-            error={!!errors.password?.password}
+            error={errors.password?.password}
             right={
               <TextInput.Icon
                 icon={hidePass ? 'eye' : 'eye-off'}
@@ -299,11 +280,6 @@ function SignUpCompany() {
         )}
         name="password.password"
       />
-      {errors.password?.password && (
-        <Text tw="text-xs text-red-600 mt-[-2] mb-2 pl-3 w-[95%]">
-          {errors.password.password.message?.toString()}
-        </Text>
-      )}
 
       {/** CONFIRM PASSWORD */}
       <Controller
@@ -312,12 +288,12 @@ function SignUpCompany() {
           required: true,
         }}
         render={({ field: { onChange, value } }) => (
-          <TextInput
+          <Input
             tw="w-full text-base bg-white rounded-sm mb-2 h-12"
             label={'Confirm Password*'}
             onChangeText={onChange}
             value={value}
-            error={!!errors.password?.confirmPassword}
+            error={errors.password?.confirmPassword}
             secureTextEntry={hideConfirmPass}
             right={
               <TextInput.Icon
@@ -329,11 +305,6 @@ function SignUpCompany() {
         )}
         name="password.confirmPassword"
       />
-      {errors.password?.confirmPassword && (
-        <Text tw="text-xs text-red-600 mt-[-2] mb-2 pl-3 w-[95%]">
-          {errors.password.confirmPassword.message?.toString()}
-        </Text>
-      )}
 
       {/** TERMS */}
       <Controller
