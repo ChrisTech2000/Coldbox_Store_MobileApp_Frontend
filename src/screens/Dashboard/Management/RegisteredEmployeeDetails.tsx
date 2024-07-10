5995efc2b31faadfb2aa8467bee7560140f2654e
Francisco Mendes <fmendes@mosano.eu>
2024-07-10 15:18:01 +0100

    the text message changes depending on the user viewing the registered employee in detail

--- src/screens/Dashboard/Management/RegisteredEmployeeDetails.tsx
+++ src/screens/Dashboard/Management/RegisteredEmployeeDetails.tsx
@@ -7,6 +7,7 @@ import { Text } from '#ui/components/Text';
 import { withSafeArea } from '#ui/primitives/withSafeArea';
 
 import type { ManagementRouteProps } from '#navigation/Dashboard/Management';
+import { useAuthStore } from '#stores/auth';
 import { useManagementStore } from '#stores/management';
 import { useApiCall } from '#services/hooks/useAPiCall';
 import ColdtivateService from '#services/ColdtivateService';
@@ -22,6 +23,7 @@ type Datum = {
 function RegisteredEmployeeDetails(props: ManagementRouteProps<'RegisteredEmployeeDetails'>) {
   const { registeredEmployeeId } = props.route.params;
 
+  const user = useAuthStore(useShallow((store) => store.user));
   const company = useManagementStore(useShallow((store) => store.company));
 
   const { data, isLoading, isValidating, refetch } = useApiCall(
@@ -53,27 +55,38 @@ function RegisteredEmployeeDetails(props: ManagementRouteProps<'RegisteredEmploy
     );
   }
 
+  const isCurrentUser = data?.user?.id === user?.id;
+
   return (
-    <View tw="flex-1 items-center justify-center space-y-4">
-      <FlatList
-        data={Object.entries(datum)}
-        keyExtractor={([key]) => `row-item-#${key}`}
-        renderItem={({ item: [key, value] }) => (
-          <React.Fragment>
-            <View tw="flex-row h-12 max-h-12 px-4">
-              <View tw="w-1/2 items-start justify-center">
-                <Text>{key}</Text>
+    <View tw="flex-1 space-y-12">
+      <View tw="h-1/2">
+        <FlatList
+          data={Object.entries(datum)}
+          keyExtractor={([key]) => `row-item-#${key}`}
+          renderItem={({ item: [key, value] }) => (
+            <React.Fragment>
+              <View tw="flex-row h-12 max-h-12 px-4">
+                <View tw="w-1/2 items-start justify-center">
+                  <Text>{key}</Text>
+                </View>
+                <View tw="w-1/2 items-start justify-center">
+                  <Text tw="truncate">{value}</Text>
+                </View>
               </View>
-              <View tw="w-1/2 items-start justify-center">
-                <Text tw="truncate">{value}</Text>
-              </View>
-            </View>
-            <Divider />
-          </React.Fragment>
-        )}
-        refreshControl={<RefreshControl refreshing={isValidating} onRefresh={refetch} />}
-        nestedScrollEnabled
-      />
+              <Divider />
+            </React.Fragment>
+          )}
+          refreshControl={<RefreshControl refreshing={isValidating} onRefresh={refetch} />}
+        />
+      </View>
+
+      <View tw="mx-4">
+        <Text tw="text-center">
+          {isCurrentUser
+            ? 'To delete your account, go to Account Details.'
+            : 'If you want to delete this account, please contact app@yourvcca.org'}
+        </Text>
+      </View>
     </View>
   );
 }
