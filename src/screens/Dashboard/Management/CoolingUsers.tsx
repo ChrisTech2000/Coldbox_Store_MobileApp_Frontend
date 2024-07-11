ca224a3185e9b223963b78ba4d4fe67a09189a97
Francisco Mendes <fmendes@mosano.eu>
2024-07-11 12:27:54 +0100

    data binding was done on the Cooling Users screen
    - the getOperatorFarmers query was used to obtain the list of cooling
    users;
    - the spinner was added to show the loading status;
    - the list of cooling users was created and has a factory to determine
    who uses the application;
    - the possibility of taking advantage of refresh control was also used
    so that the user has the possibility of revalidating the data manually.

--- src/screens/Dashboard/Management/CoolingUsers.tsx
+++ src/screens/Dashboard/Management/CoolingUsers.tsx
@@ -1,15 +1,63 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { FlatList, RefreshControl, View } from 'react-native';
+import { ActivityIndicator, Divider, List, type ListItemProps } from 'react-native-paper';
+import { useShallow } from 'zustand/react/shallow';
 
 import { withSafeArea } from '#ui/primitives/withSafeArea';
 
+import { useAuthStore } from '#stores/auth';
+import { useApiCall } from '#services/hooks/useAPiCall';
+import ColdtivateService from '#services/ColdtivateService';
+import { paperTheme } from '#ui/lib/theme';
+import type { User } from '#types/global';
+
 function CoolingUsers() {
+  const user = useAuthStore(useShallow((store) => store.user));
+
+  const { data, isLoading, isValidating, refetch } = useApiCall(
+    'getOperatorFarmers',
+    ColdtivateService.getOperatorFarmers,
+    { operator: user?.id as number },
+    {
+      skip: !user?.id,
+      defaultData: [],
+    }
+  );
+
+  if (isLoading) {
+    return (
+      <View tw="flex-1 items-center justify-center">
+        <ActivityIndicator animating color={paperTheme.colors.primary} size="large" />
+      </View>
+    );
+  }
+
   return (
-    <View tw="flex-1 items-center justify-center space-y-4">
-      <Text>Cooling Users</Text>
+    <View tw="flex-1 justify-start">
+      <FlatList
+        data={data}
+        keyExtractor={(item) => `cooling-user-item-#${item.id}`}
+        renderItem={({ item }) => (
+          <React.Fragment>
+            <List.Item {..._propsFactory(item.user)} />
+            <Divider />
+          </React.Fragment>
+        )}
+        refreshControl={<RefreshControl refreshing={isValidating} onRefresh={refetch} />}
+        nestedScrollEnabled
+      />
     </View>
   );
+}
+
+function _propsFactory(datum: User) {
+  const props = {} as ListItemProps;
+  props.title = [datum.firstName, datum.lastName].join(' ');
+  if (datum.firstName !== 'User without a phone') {
+    props.right = (props) => <List.Icon {...props} icon="cellphone" />;
+  }
+  props.onPress = () => undefined;
+  return props;
 }
 
 export default withSafeArea(CoolingUsers);
