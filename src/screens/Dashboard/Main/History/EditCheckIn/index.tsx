17cc81c96576d5418c3d32a09a3ef1bbce9f80d6
sverissimo <sverissimo@mosano.eu>
2024-07-29 10:56:32 +0000

    fix: edit check in is now working correctly

--- src/screens/Dashboard/Main/History/EditCheckIn/index.tsx
+++ src/screens/Dashboard/Main/History/EditCheckIn/index.tsx
@@ -101,8 +101,7 @@ function EditCheckIn(props: HistoryTabStackRouteProps<'EditCheckIn'>) {
         const promises = values.produces.map((produce) => {
           return ColdtivateService.editCheckIn({
             id: produce.id,
-            cropId: matchingProduces.find((_produce) => _produce.cropName === produce.crop)
-              ?.cropId as number,
+            cropId: crops.find((crop) => crop.name === produce.crop)?.id as number,
             plannedDays: Number(produce.plannedDays) as number,
             farmerId: farmer?.id as number,
           });
