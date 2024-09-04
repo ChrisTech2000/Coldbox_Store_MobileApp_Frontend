4fe90551618e78e09630c708c515a5b760d18cb1
sverissimo <sverissimo@mosano.eu>
2024-09-04 11:15:04 +0000

    fix: 'view existing' button in Edit Cooling Unit screen no longer showing when switch is turned off + empty messages added

--- src/screens/Dashboard/Management/AddCoolingUnit/components/FormFields.tsx
+++ src/screens/Dashboard/Management/AddCoolingUnit/components/FormFields.tsx
@@ -34,9 +34,11 @@ export default function FormFields(props: {
 }) {
   const { isEditMode, sensorList } = props;
 
-  const { control, formState } = FormManager.useFormManager();
+  const { control, watch, formState } = FormManager.useFormManager();
   const { t } = useTranslationUtils();
 
+  const integratedSensor = watch('sensor');
+  const sensorData = watch('sensorData');
   const errors = formState.errors;
 
   return (
@@ -80,8 +82,10 @@ export default function FormFields(props: {
         )}
       />
       <Sensors>
-        {typeof sensorList === 'undefined' || sensorList.length === 0 ? null : (
-          <TableModal datums={sensorList} />
+        {!sensorList?.length ||
+        (sensorData && sensorList[0].type !== sensorData?.type) ||
+        !integratedSensor ? null : (
+          <TableModal datums={sensorList ?? []} />
         )}
       </Sensors>
       <Controller
--- src/screens/Dashboard/Management/Locations.tsx
+++ src/screens/Dashboard/Management/Locations.tsx
@@ -3,16 +3,19 @@ import { FlatList, RefreshControl, View } from 'react-native';
 import { ActivityIndicator, Divider, List } from 'react-native-paper';
 import { useShallow } from 'zustand/react/shallow';
 
+import { Text } from '#ui/components/Text';
+import { paperTheme } from '#ui/lib/theme';
 import { withSafeArea } from '#ui/primitives/withSafeArea';
 
+import { useTranslationUtils } from '#i18n/utils';
 import type { ManagementRouteProps } from '#navigation/Dashboard/Management';
-import { useApiCall } from '#services/hooks/useAPiCall';
 import ColdtivateService from '#services/ColdtivateService';
-import { paperTheme } from '#ui/lib/theme';
+import { useApiCall } from '#services/hooks/useAPiCall';
 import { useManagementStore } from '#stores/management';
 
 function Locations(props: ManagementRouteProps<'Locations'>) {
   const { navigation } = props;
+  const { t } = useTranslationUtils();
 
   const company = useManagementStore(useShallow((store) => store.company));
 
@@ -36,27 +39,33 @@ function Locations(props: ManagementRouteProps<'Locations'>) {
 
   return (
     <View tw="flex-1 justify-start">
-      <FlatList
-        data={data}
-        keyExtractor={(item) => `location-item-#${item.id}`}
-        renderItem={({ item }) => (
-          <React.Fragment>
-            <List.Item
-              title={item.name}
-              onPress={() => {
-                navigation.navigate('EditLocation', {
-                  locationId: item.id,
-                  companyId: item.company.id,
-                });
-              }}
-              right={(props) => <List.Icon {...props} icon="chevron-right" />}
-            />
-            <Divider />
-          </React.Fragment>
-        )}
-        refreshControl={<RefreshControl refreshing={isValidating} onRefresh={refetch} />}
-        nestedScrollEnabled
-      />
+      {!data.length ? (
+        <Text tw="m-4 text-base text-center text-gray-400">
+          {t('Dashboard.Management.Location.emptyState')}
+        </Text>
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={(item) => `location-item-#${item.id}`}
+          renderItem={({ item }) => (
+            <React.Fragment>
+              <List.Item
+                title={item.name}
+                onPress={() => {
+                  navigation.navigate('EditLocation', {
+                    locationId: item.id,
+                    companyId: item.company.id,
+                  });
+                }}
+                right={(props) => <List.Icon {...props} icon="chevron-right" />}
+              />
+              <Divider />
+            </React.Fragment>
+          )}
+          refreshControl={<RefreshControl refreshing={isValidating} onRefresh={refetch} />}
+          nestedScrollEnabled
+        />
+      )}
     </View>
   );
 }
