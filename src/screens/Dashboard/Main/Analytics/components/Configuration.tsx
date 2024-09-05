bc0679ff5231f3ee6427c63bccfe38506e385f9b
sverissimo <sverissimo@mosano.eu>
2024-09-05 18:37:53 +0000

    fix: several issues related to smaller screens for UX improvement

--- src/screens/Dashboard/Main/Analytics/components/Configuration.tsx
+++ src/screens/Dashboard/Main/Analytics/components/Configuration.tsx
@@ -47,7 +47,7 @@ export function ConfigurationModal({
   return (
     <Portal>
       <Modal visible={isOpen} onDismiss={dismiss}>
-        <View tw="items-center bg-white mx-14 p-2 rounded-3xl h-auto space-y-4">
+        <View tw="items-center bg-white mx-12 p-2 rounded-3xl h-auto space-y-4">
           <Text variant="TextMedium" tw="text-base mt-2 px-2">
             {t('Dashboard.Management.UsageAnalysis.modal.title')}
           </Text>
