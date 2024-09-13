f29563710b6e96e139794bc8ee9a8016af83c8f9
Francisco Mendes <fmendes@mosano.eu>
2024-09-13 15:29:31 +0100

    built coupon modal and coupon root screen

--- src/screens/Dashboard/AccountDetails/Coupons/index.tsx
+++ src/screens/Dashboard/AccountDetails/Coupons/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Modalize } from 'react-native-modalize';
 
 import { Text } from '#ui/components/Text';
 import { Button } from '#ui/components/Button';
 import { withSafeArea } from '#ui/primitives/withSafeArea';
 
 import { paperTheme } from '#ui/lib/theme';
+import CouponModal from './components/CouponModal';
 
 function CouponsRoot() {
+  const modalRef = useRef<Modalize>(null);
+
   return (
     <View tw="py-3 flex-1 flex-col items-center justify-between">
       <View tw="w-30 h-30" />
@@ -20,13 +24,15 @@ function CouponsRoot() {
         <Text tw="text-base">No coupons has been added yet</Text>
       </View>
 
+      <CouponModal modalRef={modalRef} />
+
       <Button
         tw="w-5/6 my-4"
         mode="outlined"
         uppercase
         onPress={(evt) => {
           evt.stopPropagation();
-          // TODO -> open bottom sheet
+          modalRef.current?.open();
         }}
       >
         Add Coupon
