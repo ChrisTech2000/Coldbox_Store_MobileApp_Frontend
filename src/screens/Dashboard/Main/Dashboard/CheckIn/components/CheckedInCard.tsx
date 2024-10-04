12ba8039f535266de02cfca841e901a0dd0bb1ce
Francisco Mendes <fmendes@mosano.eu>
2024-10-04 11:05:43 +0100

    fix: screen padding

--- src/screens/Dashboard/Main/Dashboard/CheckIn/components/CheckedInCard.tsx
+++ src/screens/Dashboard/Main/Dashboard/CheckIn/components/CheckedInCard.tsx
@@ -64,9 +64,11 @@ export function CheckedInCard({
   return (
     <View tw="rounded-md border border-gray-300 mb-2">
       <View tw="flex flex-row-reverse justify-between">
-        <TouchableOpacity tw="w-5 mr-2 mt-2" onPress={openOptionsModal}>
-          <Icon source="dots-vertical" size={20} />
-        </TouchableOpacity>
+        {!checkOutCode ? (
+          <TouchableOpacity tw="w-5 mr-2 mt-2" onPress={openOptionsModal}>
+            <Icon source="dots-vertical" size={20} />
+          </TouchableOpacity>
+        ) : null}
 
         <View tw="flex flex-row space-x-1 items-center px-1">
           <FastImage
