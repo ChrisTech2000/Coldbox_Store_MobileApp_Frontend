1fde161348596eabacb35ea31b68733be3c4da4f
Francisco Mendes <fmendes@mosano.eu>
2024-10-14 18:27:56 +0100

    fix: app version modal is not dismissable if an update is pending

--- src/common/AppVersion.tsx
+++ src/common/AppVersion.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Linking, Platform, View } from 'react-native';
 import { Modal, Portal } from 'react-native-paper';
 import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -9,48 +9,69 @@ import { Button } from '#ui/components/Button';
 
 import AuthService from '#services/AuthService';
 import { useTranslationUtils } from '#i18n/utils';
-import { useToggle } from '#ui/hooks/useToggle';
 import { paperTheme } from '#ui/lib/theme';
+import { ENVIRONMENT } from '#constants/environment';
 
-function useAppVersionCheck(cb: () => void) {
+const IS_DEV_ENV = typeof ENVIRONMENT === 'string' && ENVIRONMENT === 'development';
+
+function useAppVersionCheck(cb: (needsUpdate: boolean) => void) {
   const hasRun = useRef<boolean>(false);
 
   const checkVersion = useCallback(async () => {
-    try {
-      if (hasRun.current) return;
-      hasRun.current = true;
+    if (hasRun.current) return;
+    hasRun.current = true;
 
-      async function _getBackendVersion(): Promise<number> {
-        if (Platform.OS === 'ios') return AuthService.getBackendIOSVersion();
-        return AuthService.getBackendAndroidVersion();
-      }
-
-      const result = await _getBackendVersion();
-
-      const backendVersionCode =
-        Platform.OS === 'android' ? result.toString().replace(/\./g, '0') : result.toString();
-      const appVersionCode = getBuildNumber();
-
-      if (appVersionCode < backendVersionCode) cb();
-    } catch (exception) {
-      console.error(exception);
+    async function _getBackendVersion(): Promise<number> {
+      if (Platform.OS === 'ios') return AuthService.getBackendIOSVersion();
+      return AuthService.getBackendAndroidVersion();
     }
+
+    const result = await _getBackendVersion();
+
+    const backendVersionCode =
+      Platform.OS === 'android' ? result.toString().replace(/\./g, '0') : result.toString();
+    const appVersionCode = getBuildNumber();
+
+    cb(appVersionCode < backendVersionCode);
   }, []);
 
   useEffect(() => {
-    void checkVersion();
+    try {
+      void checkVersion();
+    } catch (exception) {
+      console.error(exception);
+    }
   }, []);
 }
 
 export default function AppVersionModal() {
   const { t } = useTranslationUtils();
 
-  const [isVisible, toggleVisible] = useToggle(false);
-  useAppVersionCheck(toggleVisible);
+  const [state, setState] = useState<{ isVisible: boolean; isDismissable: boolean }>({
+    isVisible: false,
+    isDismissable: true,
+  });
+
+  useAppVersionCheck((needsUpdate) => {
+    if (IS_DEV_ENV) return;
+    return setState({
+      isDismissable: !needsUpdate,
+      isVisible: needsUpdate,
+    });
+  });
 
   return (
     <Portal>
-      <Modal visible={isVisible} onDismiss={toggleVisible}>
+      <Modal
+        dismissable={state.isDismissable}
+        visible={state.isVisible}
+        onDismiss={() => {
+          setState((state) => ({
+            ...state,
+            isVisible: false,
+          }));
+        }}
+      >
         <View tw="w-full items-center bg-white rounded-3xl w-4/5 max-w-4/5 h-auto py-4 px-5 self-center space-y-2">
           <View tw="space-y-3">
             <View tw="self-center">
@@ -71,7 +92,6 @@ export default function AppVersionModal() {
               onPress={async (evt) => {
                 try {
                   evt.stopPropagation();
-                  toggleVisible();
                   if (Platform.OS === 'android') {
                     await Linking.openURL(
                       'https://play.google.com/store/apps/details?id=com.base.coldtivate&hl=en&gl=US&pli=1'
