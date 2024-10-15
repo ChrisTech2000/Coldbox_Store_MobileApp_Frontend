2096e51f501f1730f9925043be92835abefb0335
José Moreira <josemoreiravarzim@gmail.com>
2024-10-15 23:22:09 +0100

    fix: disable toggles while the info is being updated

--- src/screens/Dashboard/AccountDetails/ContactsSharing.tsx
+++ src/screens/Dashboard/AccountDetails/ContactsSharing.tsx
@@ -16,10 +16,13 @@ function ContactsSharing() {
   const { t } = useTranslationUtils();
   const [user, setUser] = useAuthStore((store) => [store.user, store.setUser]);
   const toast = InAppNotifications.useToast();
+  const [loading, setLoading] = React.useState(false);
 
   const onPreferencesChange = useCallback(
     async (publicPhone?: boolean, publicEmail?: boolean) => {
       if (!user) return;
+      setLoading(true);
+
       try {
         const userDatum = await ColdtivateService.updateUser({
           userId: user.id,
@@ -41,9 +44,11 @@ function ContactsSharing() {
         });
       } catch (exception) {
         console.error(exception);
+      } finally {
+        setLoading(false);
       }
     },
-    [user]
+    [user, setLoading]
   );
 
   return (
@@ -60,6 +65,7 @@ function ContactsSharing() {
             )}
             right={() => (
               <Switch
+                disabled={loading}
                 value={user?.isPhonePublic ?? false}
                 onValueChange={(val) => onPreferencesChange(val, undefined)}
               />
@@ -78,6 +84,7 @@ function ContactsSharing() {
             )}
             right={() => (
               <Switch
+                disabled={loading}
                 value={user?.isEmailPublic ?? false}
                 onValueChange={(val) => onPreferencesChange(undefined, val)}
               />
