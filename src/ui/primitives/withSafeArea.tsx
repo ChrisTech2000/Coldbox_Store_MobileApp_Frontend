303a983921b295b3ae0909eb0e21e880be263e64
Francisco Mendes <fmendes@mosano.eu>
2024-10-22 12:17:20 +0100

    chore: improved previous commit

--- src/ui/primitives/withSafeArea.tsx
+++ src/ui/primitives/withSafeArea.tsx
@@ -4,7 +4,7 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 type Boundaries = 'top' | 'bottom';
 
-export const BOTTOM_NAV_HEIGHT = Platform.OS === 'android' ? 75 : 115;
+export const BOTTOM_NAV_HEIGHT = Platform.OS === 'android' ? 75 : 108;
 
 export function withSafeArea<T extends object>(
   WrappedComponent: ComponentType<T>,
