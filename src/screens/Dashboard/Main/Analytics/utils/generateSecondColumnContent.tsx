6490ffcdb47655e679619c9e90103e35f7384abf
sverissimo <sverissimo@mosano.eu>
2024-10-25 15:39:50 +0000

    fix: font size in Analytics -> Aggregated

--- src/screens/Dashboard/Main/Analytics/utils/generateSecondColumnContent.tsx
+++ src/screens/Dashboard/Main/Analytics/utils/generateSecondColumnContent.tsx
@@ -30,7 +30,7 @@ export function generateSecondColumnContent(
             height={withAmount ? 24 : 12}
             tw="self-center"
           />
-          <Text tw={cn('flex-wrap', withAmount ? 'text-lg font-bold' : 'w-24')} numberOfLines={1}>
+          <Text tw={cn('flex-wrap', withAmount ? 'text-base font-bold' : 'w-24')} numberOfLines={1}>
             {`Other${withAmount ? `: ${val}` : ''}`}
           </Text>
         </View>
@@ -57,7 +57,7 @@ export function generateSecondColumnContent(
           />
         )}
 
-        <Text tw={cn('flex-wrap', withAmount ? 'text-lg font-bold' : 'w-24')} numberOfLines={1}>
+        <Text tw={cn('flex-wrap', withAmount ? 'text-base font-bold' : 'w-24')} numberOfLines={1}>
           {`${crop?.name ?? startCase(key)}${withAmount ? `: ${Number.isFinite(+val) ? (+val % 1 === 0 ? val : (+val).toFixed(2)) : val}` : ''} ${suffix ?? ''}`}
         </Text>
       </View>
