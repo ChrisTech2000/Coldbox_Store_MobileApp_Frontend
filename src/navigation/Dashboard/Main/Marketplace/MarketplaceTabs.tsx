80ed9b6f1da35b4a30684a1caff76d6f8cec948d
Francisco Mendes <fmendes@mosano.eu>
2024-11-12 15:41:57 +0000

    chore: Marketplace Tabs - created global store to manage navigation swipe behaviour

--- src/navigation/Dashboard/Main/Marketplace/MarketplaceTabs.tsx
+++ src/navigation/Dashboard/Main/Marketplace/MarketplaceTabs.tsx
@@ -1,10 +1,13 @@
+/* eslint-disable react/prop-types */
 import type { BottomTabNavigationProp, BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 import {
   createMaterialTopTabNavigator,
   type MaterialTopTabNavigationOptions,
 } from '@react-navigation/material-top-tabs';
 import type { RouteProp } from '@react-navigation/native';
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
+import { useDebouncedCallback } from 'use-debounce';
+import { create } from 'zustand';
 
 import MarketplaceRoot from '#screens/Dashboard/Main/Marketplace';
 
@@ -38,30 +41,42 @@ const TAB_HEADERS: Record<MarketplaceTabsRoutePaths, TranslationPaths | undefine
 
 const TopTabs = createMaterialTopTabNavigator<MarketplaceTabsRoutes>();
 
+export const useMarketplaceNavigationConstraints = create<{
+  isSwipeEnabled: boolean;
+  toggle: (value?: boolean) => void;
+}>((set) => ({
+  isSwipeEnabled: false,
+  toggle: (value) => set((state) => ({ isSwipeEnabled: value ?? !state.isSwipeEnabled })),
+}));
+
 export default function MarketplaceTabs() {
   const { t } = useTranslationUtils();
+  const isSwipeEnabled = useMarketplaceNavigationConstraints((store) => store.isSwipeEnabled);
 
-  const screenOptions: ScreenOptions = useCallback((props) => {
-    // eslint-disable-next-line react/prop-types
-    const routeName = props.route.name;
-    const translationPath = TAB_HEADERS[routeName];
-    const routeTitle = translationPath ? t(translationPath) : undefined;
+  const screenOptions: ScreenOptions = useCallback(
+    (props) => {
+      const routeName = props.route.name;
+      const translationPath = TAB_HEADERS[routeName];
+      const routeTitle = translationPath ? t(translationPath) : undefined;
 
-    return {
-      ...props,
-      tabBarLabel: routeTitle,
-      tabBarIndicatorStyle: {
-        backgroundColor: paperTheme.colors.secondary,
-      },
-      tabBarStyle: {
-        backgroundColor: paperTheme.colors.background,
-      },
-      tabBarLabelStyle: {
-        color: paperTheme.colors.secondary,
-        ...paperTheme.fonts.labelMedium,
-      },
-    };
-  }, []);
+      return {
+        ...props,
+        tabBarLabel: routeTitle,
+        tabBarIndicatorStyle: {
+          backgroundColor: paperTheme.colors.secondary,
+        },
+        tabBarStyle: {
+          backgroundColor: paperTheme.colors.background,
+        },
+        tabBarLabelStyle: {
+          color: paperTheme.colors.secondary,
+          ...paperTheme.fonts.labelMedium,
+        },
+        swipeEnabled: routeName !== 'Marketplace' || isSwipeEnabled,
+      };
+    },
+    [isSwipeEnabled]
+  );
 
   // TODO: add stack for my sales
   return (
@@ -71,4 +86,53 @@ export default function MarketplaceTabs() {
       <TopTabs.Screen name="MySales" component={SalesStack} />
     </TopTabs.Navigator>
   );
+}
+
+export function useMarketplaceSwipe(filtersLength: number) {
+  const [isSwipeEnabled, setSwipeEnabled] = useMarketplaceNavigationConstraints((store) => [
+    store.isSwipeEnabled,
+    store.toggle,
+  ]);
+
+  const resetTimeout = useRef<NodeJS.Timeout | null>(null);
+
+  const debouncedHandleSwipeDisable = useDebouncedCallback(
+    useCallback(() => {
+      if (!isSwipeEnabled) return; // safe guard
+      setSwipeEnabled(false);
+      if (resetTimeout.current) clearTimeout(resetTimeout.current);
+      resetTimeout.current = setTimeout(() => {
+        setSwipeEnabled(true);
+        resetTimeout.current = null;
+      }, 500);
+    }, [isSwipeEnabled]),
+    100
+  );
+
+  const debouncedHandleSwipeEnable = useDebouncedCallback(
+    useCallback(() => {
+      if (isSwipeEnabled) return; // safe guard
+      setSwipeEnabled(true);
+      if (resetTimeout.current) {
+        clearTimeout(resetTimeout.current);
+        resetTimeout.current = null;
+      }
+    }, [isSwipeEnabled]),
+    100
+  );
+
+  useEffect(() => {
+    if (filtersLength === 0 && !isSwipeEnabled) {
+      setSwipeEnabled(true);
+    }
+    return () => {
+      if (resetTimeout.current) clearTimeout(resetTimeout.current);
+    };
+  }, [filtersLength, isSwipeEnabled, setSwipeEnabled]);
+
+  return {
+    isSwipeEnabled,
+    debouncedHandleSwipeDisable,
+    debouncedHandleSwipeEnable,
+  };
 }
