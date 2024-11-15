e421c528f0e8352d345bc527b7d16a94261743f8
Francisco Mendes <fmendes@mosano.eu>
2024-11-15 11:59:03 +0000

    fix: improved error handling

--- src/screens/Dashboard/AccountDetails/Coupons/components/CouponModal.tsx
+++ src/screens/Dashboard/AccountDetails/Coupons/components/CouponModal.tsx
@@ -9,6 +9,7 @@ import { Sup } from '#ui/components/SuperscriptText';
 import { Button } from '#ui/components/Button';
 
 import { useTranslationUtils } from '#i18n/utils';
+import InAppNotifications from '#common/InAppNotifications';
 
 type FormValues<T = string> = {
   code: string;
@@ -23,6 +24,7 @@ export default function CouponModal(props: {
   const { modalRef, datum } = props;
 
   const { t, zodResolver } = useTranslationUtils();
+  const toast = InAppNotifications.useToast();
 
   const form = useForm<FormValues>({
     defaultValues: {
@@ -53,8 +55,16 @@ export default function CouponModal(props: {
   }
 
   async function onSubmit(values: FormValues<number>) {
-    await props.onSubmit?.(values);
-    resetValues();
+    try {
+      await props.onSubmit?.(values);
+      resetValues();
+    } catch (exception) {
+      console.error(exception);
+      toast.show(t('navigation.error.errorMessage'), {
+        type: 'md_danger',
+        style: { marginBottom: 56 },
+      });
+    }
   }
 
   return (
@@ -89,7 +99,7 @@ export default function CouponModal(props: {
             />
           </View>
 
-          <View>
+          <View tw="mb-7">
             <View tw="flex-row space-x-1 mb-1.5">
               <Text tw="text-base">{t('Dashboard.Management.Coupons.percentage')}</Text>
               <Sup>(%)</Sup>
--- src/screens/Dashboard/AccountDetails/Coupons/index.tsx
+++ src/screens/Dashboard/AccountDetails/Coupons/index.tsx
@@ -35,21 +35,17 @@ function CouponsRoot() {
       <CouponModal
         modalRef={modalRef}
         onSubmit={async (values) => {
-          try {
-            await CouponService.createCoupon({
-              code: values.code,
-              discountPercentage: values.percentage,
-            });
+          await CouponService.createCoupon({
+            code: values.code,
+            discountPercentage: values.percentage,
+          });
 
-            await Promise.allSettled([
-              mutate(getQueryKey('getCouponList')),
-              mutate(getQueryKey('getCouponList', { revoked: 'included' })),
-            ]);
+          await Promise.allSettled([
+            mutate(getQueryKey('getCouponList')),
+            mutate(getQueryKey('getCouponList', { revoked: 'included' })),
+          ]);
 
-            modalRef.current?.close();
-          } catch (exception) {
-            console.error(exception);
-          }
+          modalRef.current?.close();
         }}
       />
 
