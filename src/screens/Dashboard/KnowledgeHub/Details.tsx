cc2c4740254e5e3d3be443b26b29214b44edff5d
Francisco Mendes <fmendes@mosano.eu>
2024-11-22 16:17:00 +0000

    chore: added PWA support to the Knowledge Hub root and details screens

--- src/screens/Dashboard/KnowledgeHub/Details.tsx
+++ src/screens/Dashboard/KnowledgeHub/Details.tsx
@@ -1,41 +1,22 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { WebView } from 'react-native-webview';
 
 import { withSafeArea } from '#ui/primitives/withSafeArea';
 
 import type { KnowledgeHubStackRouteProps } from '#navigation/Dashboard/KnowledgeHub';
-import { mmkv } from '#stores/lib/storage';
-
-const INJECTED_JS = `
-  (function() {
-    const header = document.querySelector('ion-header');
-    if (header) header.remove();
-
-    function removeToasts() {
-      const toasts = document.querySelectorAll('ion-toast');
-      toasts.forEach(toast => toast.remove());
-    }
-    removeToasts();
-
-    const observer = new MutationObserver(removeToasts);
-    observer.observe(document.body, { childList: true, subtree: true });
-    setTimeout(() => observer.disconnect(), 2_000);
-  })();
-`;
 
 function KnowledgeHubDetails(props: KnowledgeHubStackRouteProps<'Details'>) {
-  const ref = useRef<WebView>(null);
-
-  const language = mmkv.getString('i18n-locale');
+  const { sourceUri } = props.route.params;
 
   return (
     <WebView
-      ref={ref}
       style={{ flex: 1 }}
-      source={{ uri: `${props.route.params.sourceUri}?language=${language}` }}
-      onLoadEnd={() => {
-        ref.current?.injectJavaScript(INJECTED_JS);
-      }}
+      source={{ uri: sourceUri }}
+      cacheEnabled
+      cacheMode="LOAD_DEFAULT"
+      javaScriptEnabled
+      domStorageEnabled
+      originWhitelist={['*']}
     />
   );
 }
