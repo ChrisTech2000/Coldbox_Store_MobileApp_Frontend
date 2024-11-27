80c89700ad2b8b0e5da0cc83952323001bdef745
Francisco Mendes <fmendes@mosano.eu>
2024-11-27 16:45:26 +0000

    chore: management AddCoolingUser fields Select migration

--- src/screens/Dashboard/Management/AddCoolingUser/modules/LanguageField.tsx
+++ src/screens/Dashboard/Management/AddCoolingUser/modules/LanguageField.tsx
@@ -1,70 +1,55 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { Divider, RadioButton } from 'react-native-paper';
+import { useController } from 'react-hook-form';
 
 import { Select } from '#ui/components/Select';
 import { RadioButtonItem } from '#ui/components/RadioButton';
 import { Button } from '#ui/components/Button';
 
-import { useToggle } from '#ui/hooks/useToggle';
 import { useTranslationUtils } from '#i18n/utils';
 import { APP_LOCALES, type TranslationLocales } from '#i18n/constants';
-
-import FormManager from '../components/FormManager';
 import { cn } from '#ui/lib/cn';
 
-type Props = {
-  disabled?: boolean;
-};
+import FormManager from '../components/FormManager';
 
-export default function LanguageField(props: Props) {
-  const { watch, setValue } = FormManager.useFormManager();
-  const { t, mutate: changeLanguage } = useTranslationUtils();
+const LANGUAGES_LIST = Object.values(APP_LOCALES);
 
-  const selectedLanguage = watch('language');
+export default function LanguageField(props: { disabled: boolean }) {
+  const { control } = FormManager.useFormManager();
+  const { t } = useTranslationUtils();
 
-  const [isVisible, toggleVisibility] = useToggle(false);
-  const [internalSelection, setInternalSelection] = useState<TranslationLocales>(selectedLanguage);
+  const { field } = useController({ name: 'language', control });
+
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [internalSelection, setInternalSelection] = useState<TranslationLocales>(field.value);
 
   return (
     <View tw="mt-6">
-      <View tw="px-3">
+      <View tw="px-4 pb-2.5">
         <Select
-          disabled={props.disabled}
           variant="md"
-          label={t('languages.label')}
-          currentValue={t(['languages.options', selectedLanguage])}
-          isModalOpen={isVisible}
-          onClick={() => {
-            setInternalSelection(selectedLanguage);
-            toggleVisibility();
-          }}
-          content={{
-            header: t('languages.label'),
-            options: (
-              <RadioButton.Group
-                value={internalSelection}
-                onValueChange={(value) => setInternalSelection(value as TranslationLocales)}
-              >
-                {Object.values(APP_LOCALES).map((option, optionIdx) => (
-                  <RadioButtonItem
-                    key={`${option}-${optionIdx}`}
-                    label={t(['languages.options', option])}
-                    value={option}
-                    tw="flex flex-row-reverse ml-[-10]"
-                  />
-                ))}
-              </RadioButton.Group>
-            ),
-            footer: (
+          isOpen={isModalVisible}
+          onOpenChange={setIsModalVisible}
+          onDismiss={() => setInternalSelection(field.value)}
+          disabled={props.disabled}
+        >
+          <Select.Touchable
+            label={t('languages.label')}
+            displayValue={t(['languages.options', field.value])}
+          />
+          <Select.Dialog
+            enableScroll
+            header={t('languages.label')}
+            FooterElement={
               <View tw="flex flex-row items-center justify-end">
                 <Button
                   mode="text"
                   uppercase
                   onPress={(evt) => {
                     evt.stopPropagation();
-                    setInternalSelection(selectedLanguage);
-                    toggleVisibility();
+                    setInternalSelection(field.value);
+                    setIsModalVisible(false);
                   }}
                 >
                   {t('actions.cancel')}
@@ -74,19 +59,37 @@ export default function LanguageField(props: Props) {
                   uppercase
                   onPress={async (evt) => {
                     evt.stopPropagation();
-                    setValue('language', internalSelection);
-                    await changeLanguage(internalSelection);
-                    toggleVisibility();
+                    field.onChange(internalSelection);
+                    setIsModalVisible(false);
                   }}
                 >
                   {t('actions.ok')}
                 </Button>
               </View>
-            ),
-          }}
-        />
+            }
+          >
+            <RadioButton.Group
+              value={internalSelection}
+              onValueChange={(value) => setInternalSelection(value as TranslationLocales)}
+            >
+              <FlatList
+                scrollEnabled={false}
+                showsVerticalScrollIndicator={false}
+                data={LANGUAGES_LIST}
+                keyExtractor={(item, itemIdx) => `${item}-${itemIdx}`}
+                renderItem={({ item }) => (
+                  <RadioButtonItem
+                    label={t(['languages.options', item])}
+                    value={item}
+                    tw="flex flex-row m-0 px-0 py-2 px-6 w-full"
+                  />
+                )}
+              />
+            </RadioButton.Group>
+          </Select.Dialog>
+        </Select>
       </View>
-      <Divider tw={cn('w-full bg-gray-700 mt-2 my-3', props.disabled && 'bg-gray-300')} />
+      <Divider tw={cn('w-full bg-gray-700', props.disabled && 'bg-gray-300')} />
     </View>
   );
 }
