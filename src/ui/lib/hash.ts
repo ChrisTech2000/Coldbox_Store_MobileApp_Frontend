953a90e92c2d54a7a1cd4416e886a4dbd12405d9
Francisco Mendes <fmendes@mosano.eu>
2024-12-09 16:58:15 +0000

    chore: improve simple hash algo

--- src/ui/lib/hash.ts
+++ src/ui/lib/hash.ts
@@ -1,9 +1,10 @@
 export function stringToHash(str: string): string {
-  let hash = 0;
+  let hash = 5381;
   for (let i = 0; i < str.length; i++) {
     const char = str.charCodeAt(i);
-    hash = (hash << 5) - hash + char;
-    hash = hash & hash;
+    hash = (hash << 5) + hash + char;
+    hash = hash >>> 0;
   }
-  return hash.toString(36);
+  const lengthHash = str.length.toString(16);
+  return `${hash.toString(36)}-${lengthHash}`;
 }
