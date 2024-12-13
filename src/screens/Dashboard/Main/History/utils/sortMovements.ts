0fa16bfa60a0953548406a08330ca23c6aef87bc
sverissimo <sverissimo@mosano.eu>
2024-12-13 15:16:55 -0100

    fix: sorting function in history screen

--- src/screens/Dashboard/Main/History/utils/sortMovements.ts
+++ src/screens/Dashboard/Main/History/utils/sortMovements.ts
@@ -5,8 +5,6 @@ import { ESortingOptions } from '../components/SortMenu';
 
 type Movement = GetMovementsHistoryResponse[number];
 
-const isMarketplaceOrder = (movement: Movement) =>
-  movement.initiatedFor === EInitiatedFor.MARKETPLACE_ORDER;
 const compareByDate = (dateA: Date | undefined, dateB: Date | undefined) => {
   return new Date(dateA || 0).getTime() - new Date(dateB || 0).getTime();
 };
@@ -23,14 +21,18 @@ export function sortMovements(a: Movement, b: Movement, sorting: ESortingOptions
     case ESortingOptions.MOVEMENT_DATE_REVERSE:
       return new Date(b.date).getTime() - new Date(a.date).getTime();
     case ESortingOptions.CHECK_OUT_FIRST: {
-      if (isMarketplaceOrder(a) && !isMarketplaceOrder(b)) return 1;
-      if (!isMarketplaceOrder(a) && isMarketplaceOrder(b)) return -1;
+      if (a.initiatedFor === EInitiatedFor.CHECK_OUT && b.initiatedFor !== EInitiatedFor.CHECK_OUT)
+        return -1;
+      if (b.initiatedFor === EInitiatedFor.CHECK_OUT && a.initiatedFor !== EInitiatedFor.CHECK_OUT)
+        return 1;
 
-      return compareByDate(a.date, b.date);
+      return compareByDate(b.date, a.date);
     }
     case ESortingOptions.CHECK_IN_FIRST: {
-      if (isMarketplaceOrder(a) && !isMarketplaceOrder(b)) return 1;
-      if (!isMarketplaceOrder(a) && isMarketplaceOrder(b)) return -1;
+      if (a.initiatedFor === EInitiatedFor.CHECK_IN && b.initiatedFor !== EInitiatedFor.CHECK_IN)
+        return -1;
+      if (b.initiatedFor === EInitiatedFor.CHECK_IN && a.initiatedFor !== EInitiatedFor.CHECK_IN)
+        return 1;
 
       return compareByDate(b.date, a.date);
     }
@@ -38,26 +40,23 @@ export function sortMovements(a: Movement, b: Movement, sorting: ESortingOptions
       return 0;
   }
 }
-
 export function sortMovementCrops(movement: Movement): Array<string> {
-  let crops: string[] = [];
-
-  if (movement.initiatedFor === EInitiatedFor.CHECK_IN) {
-    crops = movement.checkin.crates.flatMap((crate) => crate.crop?.name || []);
-  } else if (movement.initiatedFor === EInitiatedFor.CHECK_OUT) {
-    crops = movement.checkout.crates.flatMap((crate) => crate.crop?.name || []);
-  } else {
-    crops = [
-      ...movement.checkin.crates.flatMap((crate) => crate.crop?.name || []),
-      ...movement.checkout.crates.flatMap((crate) => crate.crop?.name || []),
-    ];
+  let crops: string[];
+  switch (movement.initiatedFor) {
+    case EInitiatedFor.CHECK_IN:
+      crops = _getCropNames(movement.checkin);
+      break;
+    case EInitiatedFor.CHECK_OUT:
+      crops = _getCropNames(movement.checkout);
+      break;
+    default:
+      crops = [..._getCropNames(movement.checkin), ..._getCropNames(movement.checkout)];
   }
 
-  const uniqueCrops = Array.from(new Set(crops));
+  return Array.from(new Set(crops)).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+}
 
-  return uniqueCrops.sort((cropA, cropB) => {
-    const nameA = cropA.toLowerCase();
-    const nameB = cropB.toLowerCase();
-    return nameA.localeCompare(nameB);
-  });
+function _getCropNames(data: Movement['checkin'] | Movement['checkout']): Array<string> {
+  if (!data?.crates) return [];
+  return data.crates.flatMap((crate) => crate.crop?.name || []);
 }
