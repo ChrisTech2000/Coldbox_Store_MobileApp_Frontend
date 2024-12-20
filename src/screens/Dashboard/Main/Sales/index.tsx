f7e76ddc59ed4dada709964d66f7f933c7b37fdb
Francisco Mendes <fmendes@mosano.eu>
2024-12-20 11:28:00 +0000

    fix: empty strings were being rendered in the UI

--- src/screens/Dashboard/Main/Sales/index.tsx
+++ src/screens/Dashboard/Main/Sales/index.tsx
@@ -136,7 +136,7 @@ function SalesRoot() {
                 new Set<string>(
                   item.items.map((elm) => _getNameById(elm.relCropId, _extraData.crops))
                 )
-              );
+              ).filter(Boolean);
 
               return (
                 <View tw="flex-row items-center border border-solid border-zinc-300 rounded-md p-3 my-2">
