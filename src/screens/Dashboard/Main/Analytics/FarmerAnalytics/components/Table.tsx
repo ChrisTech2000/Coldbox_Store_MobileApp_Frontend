9cd45f225352dcf729e069b49c9c540359e91b97
Francisco Mendes <fmendes@mosano.eu>
2025-01-02 12:11:22 +0000

    fix: farmer analytics section header table column headers disappearing

--- src/screens/Dashboard/Main/Analytics/FarmerAnalytics/components/Table.tsx
+++ src/screens/Dashboard/Main/Analytics/FarmerAnalytics/components/Table.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View, FlatList } from 'react-native';
 import { DataTable } from 'react-native-paper';
 import colors from 'tailwindcss/colors';
 
@@ -6,7 +7,6 @@ import { Text } from '#ui/components/Text';
 import { SkiaShadow } from '#ui/primitives/SkiaShadow';
 
 import { useTranslationUtils } from '#i18n/utils';
-import { View } from 'react-native';
 
 type TableProps = {
   header: string;
@@ -37,36 +37,73 @@ export function Table({ items, header, total }: TableProps) {
   const { t } = useTranslationUtils();
 
   return (
-    <DataTable tw="py-4 px-2 min-w-full">
-      <DataTable.Header tw="bg-gray-700 rounded-t-lg h-14">
-        <DataTable.Title>
-          <Text variant="TextMedium" tw="text-white text-base">
+    <DataTable tw="py-4 px-2 w-full min-w-full">
+      <DataTable.Header tw="bg-gray-700 rounded-t-lg h-14 space-x-4 min-w-full">
+        <DataTable.Cell tw="max-w-[30%] min-w-[30%]">
+          <Text variant="TextMedium" tw="text-white text-base" numberOfLines={2}>
             {t('Dashboard.Analytics.comparisonTab.coolingUnit')}
           </Text>
-        </DataTable.Title>
-        <DataTable.Title>
-          <View>
-            <Text variant="TextMedium" tw="text-white text-base self-center">
+        </DataTable.Cell>
+        <DataTable.Cell tw="max-w-[70%] min-w-[70%]">
+          <View tw="px-2">
+            <Text variant="TextMedium" tw="text-white text-base text-center">
               {header}
             </Text>
-            <View tw="flex flex-row w-full">
-              <Text variant="TextMedium" tw="text-white text-base">
-                {t('Dashboard.Analytics.comparisonTab.cratesTab.checkedIn')} |{' '}
+            <View tw="flex flex-row justify-between w-full">
+              <Text variant="TextMedium" tw="text-white text-base text-center">
+                {t('Dashboard.Analytics.comparisonTab.cratesTab.checkedIn')}
               </Text>
-              <Text variant="TextMedium" tw="text-white text-base">
+              <Text variant="TextMedium" tw="text-white text-base text-center">
+                {' '}
+                |{' '}
+              </Text>
+              <Text variant="TextMedium" tw="text-white text-base text-center">
                 {t('Dashboard.Analytics.comparisonTab.cratesTab.checkedOut')}
               </Text>
             </View>
           </View>
-        </DataTable.Title>
+        </DataTable.Cell>
       </DataTable.Header>
       <SkiaShadow blur={4} dx={0} dy={4} color={colors.zinc[200]} borderRadius={20}>
-        {items.map((item, index) => (
-          <DataTable.Row tw="bg-white" key={`${item.coolingUnitName}-${index}`}>
-            <DataTable.Cell>{item.coolingUnitName}</DataTable.Cell>
-            <DataTable.Cell>{item.value}</DataTable.Cell>
-          </DataTable.Row>
-        ))}
+        <FlatList
+          data={items}
+          scrollEnabled={false}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={(item, itemIdx) => `${item.coolingUnitName}-${itemIdx}`}
+          renderItem={({ item }) => {
+            const checkInValue = item.value?.[0]?.toString() || '';
+            const checkOutValue = item.value?.[1]?.toString() || '';
+
+            const maxLength = Math.max(checkInValue.length, checkOutValue.length);
+            const paddedCheckIn = checkInValue.padEnd(maxLength, ' ');
+            const paddedCheckOut = checkOutValue.padEnd(maxLength, ' ');
+
+            return (
+              <DataTable.Row tw="bg-white space-x-4">
+                <DataTable.Cell tw="max-w-[30%] min-w-[30%]">
+                  <Text tw="text-wrap" numberOfLines={3}>
+                    {item.coolingUnitName}
+                  </Text>
+                </DataTable.Cell>
+
+                <DataTable.Cell tw="max-w-[70%] min-w-[70%]">
+                  <View tw="flex flex-row justify-between items-center w-full h-full px-4">
+                    <Text variant="TextMedium" tw="text-base text-center">
+                      {paddedCheckIn}
+                    </Text>
+                    <Text variant="TextMedium" tw="text-base text-center">
+                      {' '}
+                      |{' '}
+                    </Text>
+                    <Text variant="TextMedium" tw="text-base text-center">
+                      {paddedCheckOut}
+                    </Text>
+                  </View>
+                </DataTable.Cell>
+              </DataTable.Row>
+            );
+          }}
+        />
 
         <DataTable.Row tw="bg-white rounded-b-lg">
           <DataTable.Cell>
@@ -83,31 +120,39 @@ export function ExtendedTable({ items, column1, column2, total }: ExtendedTableP
 
   return (
     <DataTable tw="py-4 px-2 min-w-full">
-      <DataTable.Header tw="bg-gray-700 rounded-t-lg h-14">
-        <DataTable.Cell>
-          <Text variant="TextMedium" tw="text-white text-base">
+      <DataTable.Header tw="bg-gray-700 rounded-t-lg h-14 space-x-4 min-w-full">
+        <DataTable.Cell tw="max-w-[30%] min-w-[30%]">
+          <Text variant="TextMedium" tw="text-white text-base text-center">
             {t('Dashboard.Analytics.comparisonTab.coolingUnit')}
           </Text>
         </DataTable.Cell>
-        <DataTable.Cell tw="w-[33%]">
-          <Text tw="flex-wrap text-base text-white" numberOfLines={2}>
-            {column1}
-          </Text>
+        <DataTable.Cell tw="max-w-[15%] min-w-[15%]">
+          <Text tw="flex-wrap text-base text-white text-center">{column1}</Text>
         </DataTable.Cell>
-        <DataTable.Cell tw="w-[33%]">
-          <Text tw="flex-wrap text-base text-white" numberOfLines={2}>
+        <DataTable.Cell tw="max-w-[55%] min-w-[55%]">
+          <Text tw="flex-wrap text-base text-white text-center w-[90%]" numberOfLines={2}>
             {column2}
           </Text>
         </DataTable.Cell>
       </DataTable.Header>
       <SkiaShadow blur={4} dx={0} dy={4} color={colors.zinc[200]} borderRadius={20}>
-        {items.map((item, index) => (
-          <DataTable.Row tw="bg-white items-center" key={`${item.coolingUnitName}-${index}`}>
-            <DataTable.Cell>{item.coolingUnitName}</DataTable.Cell>
-            <DataTable.Cell>{item.column1}</DataTable.Cell>
-            <DataTable.Cell>{item.column2}</DataTable.Cell>
-          </DataTable.Row>
-        ))}
+        <FlatList
+          data={items}
+          scrollEnabled={false}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={(item, itemIdx) => `${item.coolingUnitName}-${itemIdx}`}
+          renderItem={({ item }) => (
+            <DataTable.Row tw="bg-white space-x-4">
+              <DataTable.Cell tw="max-w-[30%] min-w-[30%]">
+                <Text tw="text-wrap" numberOfLines={3}>
+                  {item.coolingUnitName}
+                </Text>
+              </DataTable.Cell>
+              <DataTable.Cell tw="max-w-[15%] min-w-[15%]">{item.column1}</DataTable.Cell>
+              <DataTable.Cell tw="max-w-[55%] min-w-[55%]">{item.column2}</DataTable.Cell>
+            </DataTable.Row>
+          )}
+        />
 
         <DataTable.Row tw="bg-white rounded-b-lg">
           <DataTable.Cell>
