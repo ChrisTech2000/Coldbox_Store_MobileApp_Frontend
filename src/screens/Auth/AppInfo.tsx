a6b62f95e4f9682e6aa452bc74b5392cbc017375
Francisco Mendes <fmendes@mosano.eu>
2025-01-03 12:36:05 +0000

    chore: added missing divider between questions

--- src/screens/Auth/AppInfo.tsx
+++ src/screens/Auth/AppInfo.tsx
@@ -50,11 +50,13 @@ function AppInfo() {
           data={datums}
           keyExtractor={(item, itemIdx) => `faq-${item.id}-#${itemIdx}`}
           renderItem={({ item }) => (
-            <List.Accordion title={item.title} id={item.id} titleNumberOfLines={4}>
-              <Text tw="text-wrap mx-8 mt-2 mb-8">{item.text}</Text>
-            </List.Accordion>
+            <React.Fragment>
+              <List.Accordion title={item.title} id={item.id} titleNumberOfLines={4}>
+                <Text tw="text-wrap mx-8 mt-2 mb-8">{item.text}</Text>
+              </List.Accordion>
+              <Divider tw="w-full bg-gray-700 my-[1.5px]" />
+            </React.Fragment>
           )}
-          ItemSeparatorComponent={Divider}
         />
       </List.AccordionGroup>
     </View>
