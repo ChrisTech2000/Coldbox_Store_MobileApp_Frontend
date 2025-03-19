1562d8fd9b8ec2f7753de5c56fb32fba622d94c3
sverissimo <sverissimo@mosano.eu>
2025-03-19 14:37:02 -0100

    fix: reintroduced table component

--- src/screens/Dashboard/Management/AddCoolingUnit/components/FormFields.tsx
+++ src/screens/Dashboard/Management/AddCoolingUnit/components/FormFields.tsx
@@ -27,16 +27,19 @@ import UnitCapacityFields from './UnitCapacityFields';
 import UnitSizeFields from './UnitSizeFields';
 import UnitTypeField from './UnitTypeField';
 import VolumeFields from './VolumeFields';
+import TableModal from './Sensors/components/Table';
 
 export default function FormFields(props: {
   isEditMode?: boolean;
   sensorList?: GetCoolingUnitResponse['sensorList'];
 }) {
-  const { isEditMode } = props;
+  const { isEditMode, sensorList } = props;
+  const { control, watch, formState } = FormManager.useFormManager();
 
-  const { control, formState } = FormManager.useFormManager();
   const { t } = useTranslationUtils();
 
+  const integratedSensor = watch('sensor');
+  const sensorData = watch('sensorData');
   const errors = formState.errors;
 
   return (
@@ -82,6 +85,12 @@ export default function FormFields(props: {
         )}
       />
       <Sensors>
+        {!sensorList?.length ||
+        (sensorData && sensorList[0].type !== sensorData?.type) ||
+        !integratedSensor ? null : (
+          <TableModal datums={sensorList ?? []} />
+        )}
+
         <SensorsList />
       </Sensors>
       <Controller
