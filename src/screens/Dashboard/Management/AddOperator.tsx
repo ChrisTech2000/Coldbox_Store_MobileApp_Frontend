4896c6f8a3f7a4e3d3f2b1823b89960b1b69338b
Francisco Mendes <fmendes@mosano.eu>
2025-03-24 14:29:34 +0000

    chore: AddOperator - updated buttons layout

--- src/screens/Dashboard/Management/AddOperator.tsx
+++ src/screens/Dashboard/Management/AddOperator.tsx
@@ -13,6 +13,7 @@ import { Checkbox } from '#ui/components/Checkbox';
 import { Input } from '#ui/components/Input';
 import { Select } from '#ui/components/Select';
 import { Text } from '#ui/components/Text';
+import HideWithKeyboardView from '#ui/components/HideWithKeyboardView';
 import { cn } from '#ui/lib/cn';
 import { paperTheme } from '#ui/lib/theme';
 import { SkiaShadow } from '#ui/primitives/SkiaShadow';
@@ -130,121 +131,126 @@ function AddOperator(props: ManagementRouteProps<'AddOperator'>) {
   }
 
   return (
-    <KeyboardAwareScrollView
-      tw="h-full pt-5 space-y-8"
-      keyboardOpeningTime={Number.MAX_SAFE_INTEGER}
-      showsVerticalScrollIndicator={false}
-    >
-      <View tw="mx-4">
-        <SkiaShadow blur={4} dx={0} dy={4} color={colors.zinc[200]} borderRadius={20}>
-          <Banner
-            visible
-            elevation={0}
-            style={{ backgroundColor: paperTheme.colors.elevation.level3 }}
-          >
-            {t('Dashboard.Management.Operators.banner')}
-          </Banner>
-        </SkiaShadow>
-
-        <Controller
-          control={control}
-          render={({ field: { onChange, value, onBlur } }) => (
-            <Input
-              tw="w-full bg-transparent mt-7 mb-2"
-              label={`${t('Auth.ForgotPassword.phoneInputLabel')}*`}
-              mode="flat"
-              dense
-              value={value}
-              onChangeText={onChange}
-              onBlur={onBlur}
-              error={errors.phoneNumber}
-            />
-          )}
-          name="phoneNumber"
-        />
-
-        <View tw="space-y-4 mx-4 mt-2 pb-2.5">
-          <Select
-            variant="md"
-            isOpen={isModalVisible}
-            onOpenChange={setIsModalVisible}
-            onDismiss={() => setInternalSelection(selectedCoolingUnits)}
-            error={!!errors.coolingUnits}
-          >
-            <Select.Touchable label={selectLabel} />
-            <Select.Dialog
-              enableScroll
-              header={t('Dashboard.Management.Operators.fields.coolingUnits')}
-              FooterElement={
-                <View tw="flex flex-row items-center justify-end">
-                  <Button
-                    mode="text"
-                    uppercase
-                    onPress={(evt) => {
-                      evt.stopPropagation();
-                      setInternalSelection(selectedCoolingUnits);
-                      setIsModalVisible(false);
-                    }}
-                  >
-                    {t('actions.cancel')}
-                  </Button>
-                  <Button
-                    mode="text"
-                    uppercase
-                    onPress={(evt) => {
-                      evt.stopPropagation();
-                      setValue('coolingUnits', internalSelection);
-                      setIsModalVisible(false);
-                    }}
-                  >
-                    {t('actions.ok')}
-                  </Button>
-                </View>
-              }
-            >
-              <FlatList
-                scrollEnabled={false}
-                showsVerticalScrollIndicator={false}
-                data={options}
-                keyExtractor={(item, itemIdx) => `cooling-unit-item-${item.id}-#${itemIdx}`}
-                renderItem={({ item }) => (
-                  <View tw="w-full flex flex-row items-center justify-between px-4 py-2">
-                    <Text tw="text-base w-[70%]" numberOfLines={2}>
-                      {item.name}
-                    </Text>
-                    <Checkbox
-                      status={internalSelection.includes(item.id) ? 'checked' : 'unchecked'}
-                      onPress={() => {
-                        setInternalSelection((prev) =>
-                          prev.includes(item.id)
-                            ? prev.filter((id) => id !== item.id)
-                            : [...prev, item.id]
-                        );
-                      }}
-                    />
-                  </View>
-                )}
-              />
-            </Select.Dialog>
-          </Select>
-        </View>
-        <Divider tw={cn('w-full bg-gray-700', !!errors.coolingUnits && 'bg-red-700 h-0.5')} />
-      </View>
-
-      <Button
-        mode="contained"
-        tw="w-2/3 self-center"
-        icon={isSubmitting ? undefined : 'account-arrow-down-outline'}
-        onPress={handleSubmit(onSubmit)}
-        disabled={isSubmitting}
+    <View tw="flex-1">
+      <KeyboardAwareScrollView
+        keyboardOpeningTime={Number.MAX_SAFE_INTEGER}
+        showsVerticalScrollIndicator={false}
       >
-        {isSubmitting ? (
-          <ActivityIndicator animating size="small" color="white" />
-        ) : (
-          t('Dashboard.Management.Operators.actions.invite')
-        )}
-      </Button>
-    </KeyboardAwareScrollView>
+        <View tw="pt-5 space-y-8">
+          <View tw="mx-4">
+            <SkiaShadow blur={4} dx={0} dy={4} color={colors.zinc[200]} borderRadius={20}>
+              <Banner
+                visible
+                elevation={0}
+                style={{ backgroundColor: paperTheme.colors.elevation.level3 }}
+              >
+                {t('Dashboard.Management.Operators.banner')}
+              </Banner>
+            </SkiaShadow>
+
+            <Controller
+              control={control}
+              render={({ field: { onChange, value, onBlur } }) => (
+                <Input
+                  tw="w-full bg-transparent mt-7 mb-2"
+                  label={`${t('Auth.ForgotPassword.phoneInputLabel')}*`}
+                  mode="flat"
+                  dense
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  error={errors.phoneNumber}
+                />
+              )}
+              name="phoneNumber"
+            />
+
+            <View tw="space-y-4 mx-4 mt-2 pb-2.5">
+              <Select
+                variant="md"
+                isOpen={isModalVisible}
+                onOpenChange={setIsModalVisible}
+                onDismiss={() => setInternalSelection(selectedCoolingUnits)}
+                error={!!errors.coolingUnits}
+              >
+                <Select.Touchable label={selectLabel} />
+                <Select.Dialog
+                  enableScroll
+                  header={t('Dashboard.Management.Operators.fields.coolingUnits')}
+                  FooterElement={
+                    <View tw="flex flex-row items-center justify-end">
+                      <Button
+                        mode="text"
+                        uppercase
+                        onPress={(evt) => {
+                          evt.stopPropagation();
+                          setInternalSelection(selectedCoolingUnits);
+                          setIsModalVisible(false);
+                        }}
+                      >
+                        {t('actions.cancel')}
+                      </Button>
+                      <Button
+                        mode="text"
+                        uppercase
+                        onPress={(evt) => {
+                          evt.stopPropagation();
+                          setValue('coolingUnits', internalSelection);
+                          setIsModalVisible(false);
+                        }}
+                      >
+                        {t('actions.ok')}
+                      </Button>
+                    </View>
+                  }
+                >
+                  <FlatList
+                    scrollEnabled={false}
+                    showsVerticalScrollIndicator={false}
+                    data={options}
+                    keyExtractor={(item, itemIdx) => `cooling-unit-item-${item.id}-#${itemIdx}`}
+                    renderItem={({ item }) => (
+                      <View tw="w-full flex flex-row items-center justify-between px-4 py-2">
+                        <Text tw="text-base w-[70%]" numberOfLines={2}>
+                          {item.name}
+                        </Text>
+                        <Checkbox
+                          status={internalSelection.includes(item.id) ? 'checked' : 'unchecked'}
+                          onPress={() => {
+                            setInternalSelection((prev) =>
+                              prev.includes(item.id)
+                                ? prev.filter((id) => id !== item.id)
+                                : [...prev, item.id]
+                            );
+                          }}
+                        />
+                      </View>
+                    )}
+                  />
+                </Select.Dialog>
+              </Select>
+            </View>
+            <Divider tw={cn('w-full bg-gray-700', !!errors.coolingUnits && 'bg-red-700 h-0.5')} />
+          </View>
+        </View>
+      </KeyboardAwareScrollView>
+
+      <HideWithKeyboardView tw="w-full flex-row items-center justify-between px-4 pb-5 pt-4 absolute bottom-0 left-0 right-0 bg-white border-t-0.5 border-gray-600 border-solid">
+        <Button
+          tw="w-full"
+          mode="contained"
+          icon={isSubmitting ? undefined : 'account-arrow-down-outline'}
+          onPress={handleSubmit(onSubmit)}
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? (
+            <ActivityIndicator animating size="small" color="white" />
+          ) : (
+            t('Dashboard.Management.Operators.actions.invite')
+          )}
+        </Button>
+      </HideWithKeyboardView>
+    </View>
   );
 }
 
